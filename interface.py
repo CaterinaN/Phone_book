175e4460e99fb713396211c567b390bfb0379147
Alexsandr <katenok.konsta@mail.ru>
2022-12-19 20:08:38 +0300

    interface

--- interface.py
+++ interface.py
@@ -1,78 +1,70 @@
 import time
 
 def error_input():
-    print('Ошибка!')
-    print('Пожалуйста введите команду соответствующую пункту меню.')
+    '''вывод ошибки'''
+
+    print('Будьте внимательны! Что-то пошло не так!')
+    print('Введите соответствующую команду!')
     time.sleep(1)
 
-
 def done_message():
-    print('Выполнено!')
-
+    print('Выполнено!\n')
 
 main_menu = \
     'Выберите пункт меню:\n\
-    1. Список контактов\n\
-    2. Поиск контакта\n\
-    3. Добавить контакт вручную\n\
-    4. Изменить контакт\n\
-    5. Импорт контактов\n\
-    6. Экспорт контактов\n\
+    1. Поиск контакта\n\
+    2. Добавить контакт\n\
+    3. Редактировать контакт\n\
+    4. Импорт контактов\n\
+    5. Экспорт контактов\n\
+    6. Телефонная книга\n\
     7. Выход'
 
-def show_contacts(data):  # 1 in menu
-    if data != []:
-        print('Список контактов:')
-        for item in range(len(data)):
-            a = data[item]['contact_id']
-            b = data[item]['surname']
-            c = data[item]['name']
-            d = data[item]['phone']
-            e = data[item]['comment']
-            print(f'{a}) {b} {c}. {d}. {e}.')
-        print(50 * "•")
-    else:
-        print('Список контактов пуст')
+def start_page(): 
+    '''вывод гллавного меню'''
 
-def start_page():  # Starting page, choose number
     print(main_menu)
-    print(50 * "═")
     print()
-    command = input('Выберите действие:  ')
-    print(50 * "_")
+    command = input('Выберите действие: ')
     return command
 
-
 def search_contact():
+    ''' поиск контакта'''
+
     search_request = input('Введите данные для поиска: ')
     print(50 * "=")
     return search_request
 
-
 def add_contact():
-    print('Добавление контакта')
+    '''добавление контакта'''
+
+    print('Добавление контакта: ')
     print(50 * "-")
-    contact_surname = input('Введите фамилию ')  # plain text
-    contact_name = input('Введите имя ')  # DD-MM-YY
-    contact_number = input('Введите номер телефона')
-    commentary = input('Комментарий')
+    contact_surname = input('Введите фамилию: ')  
+    contact_name = input('Введите имя: ')  
+    contact_number = input('Введите номер телефона: ')
+    commentary = input('Введите комментарий: ')
     contact = [{'contact_id': '', 'surname': contact_surname, 'name': contact_name, 'phone': contact_number,
                 'comment': commentary}, ]
-    return contact  # возвращение списка словаря
-
+    return contact 
 
 def change_contact():
-    print('Изменить контакт:')
+
+    ''' ввлд контакта для редактирования или удаления'''
+
+    print('Изменить контакт: ')
     print(50 * "~")
     contact_id = input('Выберите контакт для внесения изменений: ')
     return int(contact_id)
 
-
 def change_contact_content(one_contact):
+
+    ''' редактирование контакта'''
+
     while True:
-        menu_command = input('Что необходимо сделать?\n 1 - Изменить содержание \n 2 - Удалить контакт\n')
+        menu_command = input('Что необходимо сделать?\n 1 - Редактировать контакт \n 2 - Удалить контакт\n')
         if menu_command == '1':
-            print('Изменить содержание контакта:')
+            print('Редактирование контакта:')
             while True:
                 submenu_command = input(
                     'Что необходимо изменить?\n 1 - Изменить фамилию \n 2 - Изменить имя\n 3 - Изменить номер телефона\n 4 - Изменить комментарий\n')
@@ -105,28 +97,50 @@ def change_contact_content(one_contact):
             done_message()
             break
     return one_contact
-
-
-def bye_mess():  # 6 in menu
-    print('Работа завершена!')
-
+   
+def bye_mess():
+    print('Работа закончена!')
 
 def import_contacts():
-    print('Импорт контактов:')
-    print('Пожалуйста выберите формат файла для импорта:')
-    import_type = input('csv\njson\n ')
+
+    '''импорт контактов из файла'''
+
+    print('Импорт контактов: ')
+    print('Выберите формат файла для импорта: ')
+    import_type = input('1. import_phonebook.csv\n2. import_phonebook.json\n')
     return import_type
 
-
 def export_contacts():
-    print('Экспорт контактов:')
-    print('Пожалуйста выберите формат файла для экпорта:')
+
+    '''экспорт котактов в файл'''
+
+    print('Экспорт контактов: ')
+    print('Выыберите формат файла для экпорта: ')
     export_type = input('csv\njson\n')
     return export_type
 
+def show_contacts(data):  
+
+    '''вывод в консоль всего списка контактов'''
+    
+    if data != []:
+        print(50 * "*")
+        print('Список всех контактов: ')
+        for item in range(len(data)):
+            a = data[item]['contact_id']
+            b = data[item]['surname']
+            c = data[item]['name']
+            d = data[item]['phone']
+            e = data[item]['comment']
+            print(f'{a}) {b} {c}. {d}. {e}.')
+    else:
+        print('Список контактов пуст')
 
 def result_mess(done):
     if done:
         done_message()
     else:
-        print('Произошла ошибка при выполнении операции!')
+        print('При выполнении операции произошла ошибка!')
+
+def result_import(name_file):
+    print(f'Контакты успешно импортированы из {name_file}.\n')
